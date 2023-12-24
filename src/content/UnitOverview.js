66d6508006d0c1271648c40af4321e74a5a33fcc
Max Liu <maximusliu2004@gmail.com>
2023-12-24 17:48:54 -0500

    moved all directories into the mliu2023.github.io folder

---
+++ src/content/UnitOverview.js
@@ -0,0 +1,36 @@
+export const unitNames = ['Introduction to Supervised Learning and Neural Networks',
+    'Unsupervised and Other Types of Learning',
+    'Deep Learning Architectures',
+    'Applications']
+export const overview = [
+    (
+        <div className="card">
+            Machine learning algorithms learn from the training data and adjust 
+            based on feedback. The simplest type of learning is supervised 
+            learning, which is when a model trains on a labeled dataset to 
+            predict outputs given a set of inputs.
+        </div>
+    ),
+    (
+        <div className="card">
+            Unit 1 covered supervised learning, which requires labeled datasets.
+            However, a significant limitation on using labeled datasets is that 
+            labeling is expensive. As a result, there is much more unlabeled
+            data than labeled data. This section covers learning methods that can be
+            used with limited or no labeled data.
+        </div>
+    ),
+    (
+        <div className="card">
+            Not every neural network is a simple multilayer perceptron.
+            Different types of networks have been developed to learn different 
+            kinds of patterns. In this section, we discuss new architectures, 
+            many of which come from computer vision or natural langauge processing.
+        </div>
+    ),
+    (
+        <div className="card">
+            AI has tons of applications. This is not a comprehensive list, and more
+            applications will be added in the future.
+        </div>
+    )]
